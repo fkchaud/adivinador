64920b83b4bfde1d35707bd0a887491386adb821
GoldenImper <faku_imper@hotmail.com.ar>
2019-05-07 02:45:45 -0300

    Método "validarNumero" implementado

--- AdivinadorJuego/src/adivinadorjuego/Pensador.java
+++ AdivinadorJuego/src/adivinadorjuego/Pensador.java
@@ -1,10 +1,12 @@
 package adivinadorjuego;
 
+import java.util.Arrays;
+
 public class Pensador {
     
     private String numero;
 
-    public Pensador(String numero) {
+    public Pensador(String numero) throws Exception {
         validarNumero(numero);
         this.numero = numero;
     }
@@ -17,8 +19,23 @@ public class Pensador {
         throw new UnsupportedOperationException("Not supported yet.");
     }
     
-    private void validarNumero(String numero) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    private static void validarNumero(String numero) throws Exception {
+        // Todos los caracteres son numeros y si son 4 caracteres
+        if (numero.matches("\\d\\d\\d\\d")) {
+            // Todos los caracteres son distintos
+            char charArray[] = numero.toCharArray();
+            Arrays.sort(charArray);
+            
+            for (int i = 0; i < charArray.length - 1; i++) {
+                // Comparo cada caracter con el siguiente. Como están ordenados,
+                // si hay caracteres iguales estarán juntos.
+                if (charArray[i] == charArray[i+1]) {
+                    throw new Exception("Ingrese dígitos distintos.");
+                }
+            }
+        } else {
+            throw new Exception("Ingrese 4 dígitos.");
+        }
     }
     
     public String getNumero() {
